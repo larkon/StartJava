1ff9cf70d215f58fbb257e1b83526e211dc580ab
larkon <428000@gmail.com>
2021-01-16 14:56:02 +0300

    add Variable.java
    add Variable.java

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {   
+    public static void main(String[] args) {
+        int Cpu = 3770;
+        char CpuIndex = 'K';
+        byte CpuCores = 4;
+        short CpuThreads = 8;
+        long AverageCpuMark = 6362;
+        float CpuTurboSpeed = 3.9f;
+        double CPUmarkPrice= 36.35;
+        boolean PCisWorking = true;
+
+        System.out.println("CPU: " + Cpu);
+        System.out.println("CPU Index: " + CpuIndex);
+        System.out.println("CPU Cores: " + CpuCores);
+        System.out.println("CPU Threads: " + CpuThreads);
+        System.out.println("CPU AverageCpuMark: " + AverageCpuMark);
+        System.out.println("CPU TurboSpeed: " + CpuTurboSpeed);
+        System.out.println("CPU Mark Price: " + CPUmarkPrice);
+        System.out.println("PC is Working: " + PCisWorking);
+    }
+}
