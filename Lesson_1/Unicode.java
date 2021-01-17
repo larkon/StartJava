ef56d5cd076ecc5c993e09db227ab470c5c28fff
larkon <428000@gmail.com>
2021-01-17 21:50:01 +0300

    create  new Unicode
    create  new Unicode

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,8 +1,10 @@
-import java.util.*;
+//import java.util.*;
 public class Unicode {
     public static void main(String[] args) {
+        //char unicode;
         //unicode
         for (char i=33; i<=126; i++ ) {
+            //unicode = (char)(i);
             System.out.println(i);
         };
         }
