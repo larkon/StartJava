f923c8f40a90d54f258f70948d39b887fb2612c1
larkon <428000@gmail.com>
2021-01-17 12:19:27 +0300

    create degree of
    create degree of (степень числа)

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -6,9 +6,9 @@ public class Calculator {
         char operation;
 
         //Арифметическое выражение
-        nomberOne = 170;
-        nomberTow = 10;
-        operation = '+';
+        nomberOne = 16;
+        nomberTow = 2;
+        operation = '^';
 
         //Калькулятор
         if (operation == ('+')) {System.out.println(nomberOne + nomberTow);} else
@@ -16,6 +16,14 @@ public class Calculator {
         if (operation == ('*')) {System.out.println(nomberOne * nomberTow);} else
         if (operation == ('/')) {System.out.println(nomberOne / nomberTow);} else
         if (operation == ('%')) {System.out.println(nomberOne % nomberTow);} else
+        if (operation == ('^')) {
+            int result = 1;
+            for (int i=1; i<=nomberTow; i++) {
+                result = result * nomberOne;
+                //System.out.println(result);
+            };
+            System.out.println(result);
+        } else
         {System.out.println("операция не определена");}
 
     }
