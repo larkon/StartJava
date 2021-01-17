5520972b950dcdb64b497c8e8e5f6828e38962f4
larkon <428000@gmail.com>
2021-01-17 17:06:01 +0300

    Create Unicode
    Create Unicode V1

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Unicode {
+    public static void main(String[] args) {
+        //unicode
+        for (char i=33; i<=126; i++ ) {
+            System.out.println(i);
+        };
+        }
+}
